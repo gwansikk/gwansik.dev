00f4d5ffed6e5056b778a4d9a623a0b3f66a9af2
gwansikk <gwansik.kim@gwansik.dev>
2024-10-09 00:52:39 +0900

    chore: fix typo name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center min-h-screen gap-4">
-      <h1 className="font-semibold">GwanSik Kim</h1>
+      <h1 className="font-semibold">Gwansik Kim</h1>
       <a href="https://github.com/gwansikk" className="underline underline-offset-2">
         GitHub
       </a>
