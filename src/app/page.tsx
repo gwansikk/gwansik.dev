2106b91f40c8afb4683fec03a7545d4b08a50a4f
gwansikk <gwansik.kim@gwansik.dev>
2024-11-08 02:38:52 +0900

    feat: update main contents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,14 @@ import List from '@/app/components/List';
 import Title from '@/app/components/Title';
 import HyperLink from '@/app/components/HyperLink';
 
-import { SiReactquery, SiPnpm, SiVitepress } from 'react-icons/si';
-import { IoDocument } from 'react-icons/io5';
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import {
+  SiReactquery,
+  SiPnpm,
+  SiVitepress,
+  SiVite,
+  SiGraphql,
+} from 'react-icons/si';
+import { FaAddressCard, FaGithub, FaLinkedin } from 'react-icons/fa';
 import { FaXTwitter } from 'react-icons/fa6';
 
 export default function Home() {
@@ -19,21 +24,23 @@ export default function Home() {
         <p>안녕하세요, 개발자 김관식입니다.</p>
         <p>
           저는 TypeScript 기반으로 Node.js 런타임 환경에서 프로덕트를 개발하며,
-          풀타임 작업 외 시간에는 프론트엔드 오픈소스 활동을 하고 있어요.
+          풀타임 작업 시간 외에는 프론트엔드 오픈소스 컨트리뷰터로 활동하고
+          있어요.
         </p>
         <p>
           주변에서 겪는 문제를 기술로 해결하고자 창업과 개발을 시작했어요.
-          사건을 해결하는 프로파일러처럼, 주도적으로 문제를 분석하고 적절한
-          정답을 찾아 해결하는 데 노력하며 작업의 임팩트와 전체적인 기여를
+          사건을 해결하는 프로파일러처럼 주도적으로 문제를 분석하고 적절한
+          정답을 찾아 해결하는 과정을 즐기며, 작업의 임팩트와 전체적인 기여를
           중요하게 생각합니다.
         </p>
         <p>
-          영향력을 확장하여 개발 생태계에 관심을 가지고 오픈소스 활동을
-          하고있어요. 현재 Suspensive의 관리자로 활동하며 TanStack Query, pnpm
-          등 여러 프로젝트에 기여한 경험이 있습니다.
+          개발 생태계에 관심을 가지고 오픈소스 활동을 하고 있어요. 현재{' '}
+          <HyperLink href='https://suspensive.org'>Suspensive</HyperLink>의
+          관리자로 라이브러리를 개발하고 있으며 TanStack Query, pnpm 등 여러
+          프로젝트에 기여한 경험이 있습니다.
         </p>
       </section>
-      <Section title='다음 프로젝트를 관리하거나 기여하고 있어요.'>
+      <Section title='관리하거나 기여하고 있어요.'>
         <List>
           <HyperLink href='https://suspensive.org'>Suspensive</HyperLink>
           <HyperLink href='https://query-adaptor.gwansik.dev'>
@@ -61,9 +68,9 @@ export default function Home() {
           </HyperLink>
         </List>
       </Section>
-      <Section title='다음 채널에서 저의 활동을 살펴볼 수 있어요.'>
+      <Section title='저의 활동을 살펴볼 수 있어요.'>
         <List>
-          <HyperLink icon={<IoDocument />} href='resume'>
+          <HyperLink icon={<FaAddressCard />} href='resume'>
             Resume
           </HyperLink>
           <HyperLink icon={<FaGithub />} href='https://github.com/gwansikk'>
@@ -85,10 +92,21 @@ export default function Home() {
           </HyperLink>
         </List>
       </Section>
+      <Section title='관심있게 연구하고 있어요.'>
+        <List>
+          <HyperLink icon={<SiVite />} href='https://vite.dev'>
+            Vite
+          </HyperLink>
+          <HyperLink icon={<SiGraphql />} href='https://graphql.org'>
+            GraphQL
+          </HyperLink>
+          <HyperLink href='https://effect.website'>Effect</HyperLink>
+        </List>
+      </Section>
       <section>
         <p>
-          더 이야기 나누고 싶다면 gwansik.kim@gwansik.dev 메일로 언제든지
-          연락주세요.
+          더 이야기를 나누고 싶다면 gwansik.kim@gwansik.dev 메일로 언제든지 연락
+          주세요.
         </p>
         <p>감사합니다!</p>
       </section>
