5e926013ee51b76500bcb410fcc8b09dddae527b
gwansikk <gwansik.kim@gwansik.dev>
2024-11-15 22:40:01 +0900

    feat: add metadata for Notes page

--- src/app/(article)/notes/page.tsx
+++ src/app/(article)/notes/page.tsx
@@ -1,6 +1,11 @@
 import Title from '@/app/components/title';
 import Description from '@/app/components/description';
 import PreviousButton from '@/app/components/previous-button';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Notes - Gwansik Kim',
+};
 
 export default function Notes() {
   return (
