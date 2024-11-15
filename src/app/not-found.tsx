3c3462ddc0fe4c2e12672d2efe222727fad60a01
gwansikk <gwansik.kim@gwansik.dev>
2024-11-15 22:39:51 +0900

    refactor: remove href from PreviousButton in NotFound

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,12 @@
 import Title from '@/app/components/title';
 import PreviousButton from './components/previous-button';
-import { PATH } from './constants';
 
 export default function NotFound() {
   return (
     <>
       <Title>404</Title>
       <p>요청하신 페이지를 찾을 수 없습니다.</p>
-      <PreviousButton href={PATH.ROOT} />
+      <PreviousButton />
     </>
   );
 }
