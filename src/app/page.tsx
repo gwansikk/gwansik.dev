b1be8119c1a491ac97aa9b2749c3b6ce0e38f963
gwansikk <gwansik.kim@gwansik.dev>
2024-12-22 17:08:30 +0900

    docs: update introduce content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,19 +22,19 @@ export default function Home() {
         <p>안녕하세요, 개발자 김관식입니다.</p>
         <p>
           저는 TypeScript 기반으로 Node.js 런타임 환경에서 프로덕트를 개발하며,
-          풀타임 작업 외에는 프론트엔드 오픈소스 메이커로 활동하고 있어요.
+          풀타임 작업 외에는 오픈 소스 개발 활동을 하고 있습니다.
         </p>
         <p>
-          주변에서 겪는 문제를 기술로 해결하고자 창업과 개발을 시작했어요.
+          주변에서 겪는 문제를 기술로 해결하고자 창업과 개발을 시작했습니다.
           사건을 해결하는 프로파일러처럼 주도적으로 문제를 분석하고 적절한
           해답을 찾아가는 과정을 즐기며, 작업의 임팩트와 전체적인 기여를
           중요하게 생각합니다.
         </p>
         <p>
-          개발 생태계에 영향력을 주고자 오픈 소스 활동을 하고 있어요. 현재{' '}
-          <Anchor href="https://suspensive.org">Suspensive</Anchor>의 관리자로
-          라이브러리를 개발하고 있으며 TanStack Query, es-toolkit, pnpm 등 여러
-          프로젝트에 기여한 경험이 있습니다.
+          개발 생태계에 영향력을 주고자 오픈 소스에 큰 관심을 가지고 있습니다.
+          현재 <Anchor href="https://suspensive.org">Suspensive</Anchor>의
+          관리자로 라이브러리를 개발하고 있으며 TanStack Query, es-toolkit, pnpm
+          등 여러 프로젝트에 기여한 경험이 있습니다.
         </p>
       </section>
       <Section title="해당 프로젝트를 관리하거나 기여하고 있어요.">
