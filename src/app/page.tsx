92b2b50415353f52e2da2ea2360779d24c8fb02b
gwansikk <gwansik.kim@gwansik.dev>
2025-01-02 11:32:54 +0900

    feat: update introduction and add recent posts section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Section from '@/components/section';
 import List from '@/components/list';
 import Title from '@/components/title';
 import Anchor from '@/components/anchor';
-
 import {
   SiReactquery,
   SiPnpm,
@@ -10,6 +9,9 @@ import {
   SiLinkedin,
   SiX,
 } from 'react-icons/si';
+import { getPosts } from '@/data/velite-data-accessor';
+
+const POSTS = getPosts().slice(0, 2);
 
 export default function Home() {
   return (
@@ -19,7 +21,7 @@ export default function Home() {
         <h2 className="text-zinc-500">Frontend Engineer</h2>
       </header>
       <section className="space-y-6">
-        <p>안녕하세요, 개발자 김관식입니다.</p>
+        <p>안녕하세요, 엔지니어 김관식입니다.</p>
         <p>
           저는 TypeScript 기반으로 Node.js 런타임 환경에서 프로덕트를 개발하며,
           풀타임 작업 외에는 오픈 소스 개발 활동을 하고 있습니다.
@@ -27,7 +29,7 @@ export default function Home() {
         <p>
           주변에서 겪는 문제를 기술로 해결하고자 창업과 개발을 시작했습니다.
           사건을 해결하는 프로파일러처럼 주도적으로 문제를 분석하고 적절한
-          해답을 찾아가는 과정을 즐기며, 작업의 임팩트와 전체적인 기여를
+          해답을 찾아가는 과정을 즐기며, 비즈니스 임팩트와 전체적인 기여를
           중요하게 생각합니다.
         </p>
         <p>
@@ -35,6 +37,15 @@ export default function Home() {
           현재 <Anchor href="https://suspensive.org">Suspensive</Anchor>의
           관리자로 라이브러리를 개발하고 있으며 TanStack Query, es-toolkit, pnpm
           등 여러 프로젝트에 기여한 경험이 있습니다.
+        </p>
+        <p>
+          더 이야기를 나누고 싶다면{' '}
+          <Anchor href="mailto:gwansik.kim@gwansik.dev">
+            gwansik.kim@gwansik.dev
+          </Anchor>
+          로 언제든지 연락 주세요.
+          <br />
+          감사합니다.
         </p>
       </section>
       <Section title="해당 프로젝트를 관리하거나 기여하고 있어요.">
@@ -60,19 +71,10 @@ export default function Home() {
           >
             pnpm
           </Anchor>
-          {/* <Anchor
-            icon={<SiRescript />}
-            href="https://github.com/rescript-lang/rescript/issues?q=involves%3Agwansikk"
-          >
-            ReScript
-          </Anchor> */}
         </List>
       </Section>
       <Section title="저의 활동을 살펴볼 수 있어요.">
         <List>
-          {/* <Anchor icon={<FaAddressCard />} href="resume">
-            Resume
-          </Anchor> */}
           <Anchor icon={<SiGithub />} href="https://github.com/gwansikk">
             GitHub
           </Anchor>
@@ -92,13 +94,13 @@ export default function Home() {
           </Anchor>
         </List>
       </Section>
-      <section>
-        <p>
-          더 이야기를 나누고 싶다면 gwansik.kim@gwansik.dev 메일로 언제든지 연락
-          주세요.
-        </p>
-        <p>감사합니다!</p>
-      </section>
+      <Section title="최근에 작성한 포스트예요.">
+        {POSTS.map((post) => (
+          <List key={post.slug}>
+            <Anchor href={post.permalink}>{post.title}</Anchor>
+          </List>
+        ))}
+      </Section>
     </>
   );
 }
