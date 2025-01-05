a7b22aff5e3f3e594e16f7260e876369c0d678f2
gwansikk <gwansik.kim@gwansik.dev>
2025-01-05 15:32:13 +0900

    feat: add profile, sign to nav

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { LuGithub } from 'react-icons/lu';
 import { PATH } from '@/constants/path';
+import Image from 'next/image';
 
 interface Props {
   href: string;
@@ -20,9 +21,25 @@ const NavButton = ({ href, children }: Props) => {
 
 const Nav = () => {
   return (
-    <nav className="container flex items-center justify-between py-2">
-      <Link href={PATH.ROOT} className="text-zinc-800 dark:text-zinc-200">
-        gwansikk
+    <nav className="container flex h-[40px] items-center justify-between">
+      <Link
+        href={PATH.ROOT}
+        className="flex items-center gap-1 text-zinc-800 dark:text-zinc-200"
+      >
+        <Image
+          src="/profile.webp"
+          alt="gwansikk-profile"
+          width={32}
+          height={32}
+          priority
+        />
+        <Image
+          src="/sign.webp"
+          alt="gwansikk-sign"
+          width={42}
+          height={24}
+          priority
+        />
       </Link>
       <div className="flex items-center gap-4">
         <NavButton href={PATH.POSTS}>Posts</NavButton>
