02f5ef63e569ff8027e907f73b52f54c339f1f95
gwansikk <gwansik.kim@gwansik.dev>
2025-01-09 12:12:34 +0900

    docs: update introduce

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,22 +21,27 @@ export default function Home() {
         <h2 className="text-zinc-500">Frontend Engineer</h2>
       </header>
       <section className="space-y-6">
-        <p>안녕하세요, 개발자 김관식입니다.</p>
-        <p>
-          저는 TypeScript 기반으로 Node.js 런타임 환경에서 비즈니스를 실현하며,
-          풀타임 작업 외에는 오픈소스 개발 활동을 하고 있습니다.
-        </p>
+        <p>개발자 김관식입니다.</p>
         <p>
           주변에서 겪는 문제를 기술로 해결하고자 창업과 개발을 시작했습니다.
-          사건을 해결하는 프로파일러처럼 주도적으로 문제를 분석하고 적절한
-          해답을 찾아가는 과정을 즐기며, 비즈니스 임팩트와 전체적인 기여를
-          중요하게 생각합니다.
+          문제를 해결하는 프로파일러처럼 주도적으로 분석하고 적절한 해답을
+          찾아가는 과정을 즐기며, 비즈니스 임팩트와 데이터 기반 사고를 통해
+          프로덕트를 성공으로 연결시키는 데에 집중합니다.
         </p>
         <p>
-          개발 생태계에 영향력을 주고자 오픈소스에 큰 관심을 가지고 있습니다.
-          현재 <Anchor href="https://suspensive.org">Suspensive</Anchor>의
-          관리자로 라이브러리를 개발하고 있으며 TanStack Query, es-toolkit, pnpm
-          등 여러 프로젝트에 기여하고 있습니다.
+          아이디어와 성장은 개발 생태계에서 비롯된다고 생각하여 오픈 소스에
+          관심이 많습니다. 현재{' '}
+          <Anchor href="https://suspensive.org">Suspensive</Anchor>의 관리자로
+          라이브러리를 개발하고 있으며{' '}
+          <Anchor href="https://github.com/TanStack/query/issues?q=involves%3Agwansikk">
+            TanStack Query
+          </Anchor>
+          , es-toolkit, pnpm 등 여러 프로젝트에 기여하고 있습니다.
+        </p>
+        <p>
+          혁신은 더하는 것보다 빼는 것에서 시작된다고 믿습니다. 복잡한 것보단
+          단순함을 좋아하며, 단순하면서도 잘 만들어진 프로덕트에서 큰 매력과
+          성취감을 느낍니다.
         </p>
         <p>
           더 이야기를 나누고 싶다면{' '}
@@ -48,14 +53,14 @@ export default function Home() {
           감사합니다.
         </p>
       </section>
-      <Section title="최근에 작성한 포스트예요.">
+      <Section title="최근에 작성한 포스트예요">
         {POSTS.map((post) => (
           <List key={post.slug}>
             <Anchor href={post.permalink}>{post.title}</Anchor>
           </List>
         ))}
       </Section>
-      <Section title="해당 프로젝트를 관리하거나 기여하고 있어요.">
+      <Section title="해당 프로젝트를 관리하거나 기여하고 있어요">
         <List>
           <Anchor href="https://suspensive.org">Suspensive</Anchor>
           <Anchor href="https://query-adaptor.gwansik.dev">
@@ -80,7 +85,7 @@ export default function Home() {
           </Anchor>
         </List>
       </Section>
-      <Section title="저의 활동을 살펴볼 수 있어요.">
+      <Section title="저의 활동을 살펴볼 수 있어요">
         <List>
           <Anchor icon={<SiGithub />} href="https://github.com/gwansikk">
             GitHub
