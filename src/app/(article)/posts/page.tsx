66aaa18732b971965fcaf562ee98d1d50f1febe1
gwansikk <gwansik.kim@gwansik.dev>
2025-02-09 16:13:00 +0900

    fix: article metadata display

--- src/app/(article)/posts/page.tsx
+++ src/app/(article)/posts/page.tsx
@@ -36,8 +36,7 @@ const Post = ({ post }: { post: TPost }) => {
         className="flex flex-col transition-colors hover:text-black sm:flex-row sm:items-center sm:gap-2 dark:hover:text-white"
       >
         <p className="font-semibold">{post.title}</p>
-        <div className="flex items-center gap-2 sm:justify-center">
-          <span className="hidden text-zinc-500 sm:flex">·</span>
+        <div className="flex gap-2 sm:justify-center">
           <ArticleMetadata date={post.date} readingTime={post.readingTime} />
         </div>
       </Link>
