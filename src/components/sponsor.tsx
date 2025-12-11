b6cc31001ec5f331c8e674439d2283f98838bff0
gwansikk <gwansik.kim@gwansik.dev>
2025-12-11 17:27:00 +0900

    fix: handle image loading error in Sponsor component

--- src/components/sponsor.tsx
+++ src/components/sponsor.tsx
@@ -1,11 +1,16 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 
 interface Props {
   github: string;
 }
 
 export function Sponsor({ github }: Props) {
+  const [imageError, setImageError] = useState(false);
+
   return (
     <Link
       className="flex flex-col items-center gap-1 text-sm"
@@ -13,15 +18,20 @@ export function Sponsor({ github }: Props) {
       target="_blank"
       rel="noopener noreferrer"
     >
-      <Image
-        className="size-11 rounded-full"
-        width={44}
-        height={44}
-        src={`https://github.com/${github}.png`}
-        alt={`${github}'s avatar`}
-        title={`sponsor by ${github}`}
-        priority
-      />
+      {imageError ? (
+        <div className="size-11 rounded-full border" />
+      ) : (
+        <Image
+          className="size-11 rounded-full"
+          width={44}
+          height={44}
+          src={`https://github.com/${github}.png`}
+          alt={`${github}'s avatar`}
+          title={`sponsor by ${github}`}
+          priority
+          onError={() => setImageError(true)}
+        />
+      )}
       <span>{github}</span>
     </Link>
   );
