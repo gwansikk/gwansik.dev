6231bd099410e3f71e1463ff5b528c8525037dfa
gwansikk <gwansik.kim@gwansik.dev>
2025-12-11 17:26:55 +0900

    chore: update intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,7 @@ import { Section } from '~/components/section';
 import { List } from '~/components/list';
 import { Title } from '~/components/title';
 import { Anchor } from '~/components/anchor';
-import {
-  SiReactquery,
-  SiPnpm,
-  SiGithub,
-  SiLinkedin,
-  SiX,
-} from 'react-icons/si';
+import { SiReactquery, SiGithub, SiLinkedin, SiX } from 'react-icons/si';
 import { getSponsors, getTalks } from '~/utils/data-access-layer';
 import { Sponsor } from '~/components/sponsor';
 
@@ -23,13 +17,10 @@ export default function Home() {
         <h2 className="font-medium text-zinc-500">Software Engineer</h2>
       </header>
       <section className="space-y-6">
-        <p>안녕하세요, 소프트웨어 엔지니어 김관식입니다.</p>
         <p>
-          주변에서 겪는 문제를 기술로 해결하고자 창업과 개발을 시작했습니다.
+          안녕하세요, 엔지니어 김관식입니다.
           <br />
-          사건을 해결하는 프로파일러처럼 분석하고 적절한 해답을 찾아가는 과정을
-          즐기며, 데이터 기반 사고를 통해 프로덕트 성장을 이끄는 데에
-          집중합니다.
+          주변에서 겪는 문제를 기술로 해결하고자 창업과 개발을 시작했습니다.
         </p>
         <p>
           미국 시장에서 제품의 기획부터 개발, 출시까지 주도한 경험이 있습니다.
@@ -37,14 +28,14 @@ export default function Home() {
           달성했습니다.
         </p>
         <p>
-          오픈 소스 생태계에 꾸준히 기여하고 있습니다. 현재{' '}
+          오픈 소스 생태계에 기여하고 있습니다. 현재{' '}
           <Anchor href="https://suspensive.org">Suspensive</Anchor> 라이브러리
           관리자로 활동하고 있으며, 개발팀{' '}
           <Anchor href="https://www.offlegacy.org">OffLegacy</Anchor>를 운영하고
           있습니다.
         </p>
       </section>
-      <Section title="해당 프로젝트에 몰입하고 있어요">
+      <Section title="프로젝트에 참여하고 있어요">
         <List>
           <Anchor href="https://www.offlegacy.org">OffLegacy</Anchor>
           <Anchor href="https://suspensive.org">Suspensive</Anchor>
